0e8a0c955ccaf1e9f4937c71b619c508fc52874f
Danil Tolkachev <dandur.oak@gmail.com>
2024-08-30 11:11:48 +0300

    fix weird bug with two extra clicks

--- quantum/pointing_device/pointing_device_drivers.c
+++ quantum/pointing_device/pointing_device_drivers.c
@@ -165,7 +165,8 @@ report_mouse_t azoteq_iqs5xx_get_report(report_mouse_t mouse_report) {
                 temp_report.buttons = pointing_device_handle_buttons(temp_report.buttons, true, POINTING_DEVICE_BUTTON1);
             } else if (base_data.gesture_events_1.two_finger_tap) {
                 pd_dprintf("IQS5XX - Two finger tap.\n");
-                temp_report.buttons = pointing_device_handle_buttons(temp_report.buttons, true, POINTING_DEVICE_BUTTON2);
+                if (base_data.previous_cycle_time == 10) // this fixes weird bug with two extra clicks
+                    temp_report.buttons = pointing_device_handle_buttons(temp_report.buttons, true, POINTING_DEVICE_BUTTON2);
             } else if (base_data.gesture_events_0.swipe_x_neg) {
                 pd_dprintf("IQS5XX - X-.\n");
                 temp_report.buttons = pointing_device_handle_buttons(temp_report.buttons, true, POINTING_DEVICE_BUTTON4);
