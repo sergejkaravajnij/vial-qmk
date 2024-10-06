e8a3ac746ef960d2b70649c87696b970cdb47037
Danil Tolkachev <dandur.oak@gmail.com>
2024-10-06 18:16:52 +0300

    fix display process layer state

--- keyboards/ergohaven/macropad/rev2/rev2.c
+++ keyboards/ergohaven/macropad/rev2/rev2.c
@@ -9,7 +9,13 @@ void keyboard_post_init_user(void) {
     display_init_kb();
 }
 
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    display_process_layer_state(get_highest_layer(layer_state | state));
+    return state;
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-    display_process_layer_state(get_current_layer());
+    display_process_layer_state(get_highest_layer(state | default_layer_state));
     return state;
 }
