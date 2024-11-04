4646b3aa0663c3fe1367f638dda22fc6257ed77d
Danil Tolkachev <dandur.oak@gmail.com>
2024-11-04 17:26:55 +0300

    set RAW_ENABLE for all keyboards

--- keyboards/ergohaven/planeta/rules.mk
+++ keyboards/ergohaven/planeta/rules.mk
@@ -22,6 +22,7 @@ DYNAMIC_MACRO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
+RAW_ENABLE = yes
 
 SRC += keyboards/ergohaven/ergohaven_main.c
 SRC += keyboards/ergohaven/ergohaven_ruen.c
